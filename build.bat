4695d7f03e7994cce95a8fcd6f0c7771cc8b8761
HoangDMHE191087 - DƯƠNG MINH HOÀNG <thuy.ntt0217@arena-multimedia.edu.vn>
2026-06-03 01:26:41 +0700

    HoangDMHE91087 - fix all bug auth and home page, anh build run bat

--- build.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-javac -encoding UTF-8 -cp "web/WEB-INF/lib/*;C:/Users/Admin/Downloads/apache-tomcat-10.1.55-windows-x64/apache-tomcat-10.1.55/lib/*" -d build/web/WEB-INF/classes @sources.txt
