301554d65c96d050febc45b5ae59951b7ee37141
chubaokhang1611-beep <chubaokhang1611@gmail.com>
2026-06-13 16:21:03 +0700

    thêm thư viện Map và HashMap

--- src/java/com/fruitmkt/dao/ProductVariantDAO.java
+++ src/java/com/fruitmkt/dao/ProductVariantDAO.java
@@ -9,8 +9,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.logging.Logger;
 
 /**
